e40d88f7d1486e2b1dcb38b2f4b2f49d1b0b0a85
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-12-02 10:12:01 +0800

    添加快速部署预览文档和功能

---
+++ quickstart.md
@@ -0,0 +1,15 @@
+# 快速部署预览
+
+## 准备 Python 环境
+
+    pipenv install
+    pipenv shell
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py createsuperuser
+    python manage.py runserver
+
+访问： http://127.0.0.1:8000/ 即可
+
+- 默认管理员： admin
+- 密码： !qaz@wsx
--- sandboxMP/settings.py
+++ sandboxMP/settings.py
@@ -96,13 +96,17 @@ WSGI_APPLICATION = 'sandboxMP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sandboxMP',
-        'HOST': '127.0.0.1',
-        'USER': 'ddadmin',
-        'PASSWORD': '1234@abcd.com',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'sandboxMP',
+    #     'HOST': '127.0.0.1',
+    #     'USER': 'ddadmin',
+    #     'PASSWORD': '1234@abcd.com',
+    #     'PORT': '3306'
+    # }
 }
 
 # Password validation
